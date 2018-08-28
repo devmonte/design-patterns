2025723dbceaa4139e27605b0a7b8689cdc32bb6
monte <greg.jonca@gmail.com>
2018-08-28 07:05:17 +0200

    Add missed field

--- TS/Observer/ICryptoCurrency.ts
+++ TS/Observer/ICryptoCurrency.ts
@@ -2,6 +2,7 @@ import { IObserver } from "./IObserver";
 
 export interface ICryptoCurrency
 {
+    price: number;
     changePrice(newPrice: number): void;
     attach(observer: IObserver): void;
     detach(observer: IObserver): void;
