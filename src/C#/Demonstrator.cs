8dc888f911b53e4b717e476cdd8e2af84f21018d
monte <greg.jonca@gmail.com>
2019-12-08 20:47:12 +0100

    Add observer pattern based on ms docs

--- src/C#/Demonstrator.cs
+++ src/C#/Demonstrator.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DesignPatterns.Mediator;
+﻿using DesignPatterns.Mediator;
+using DesignPatterns.Observer;
 
 namespace DesignPatterns
 {
@@ -18,6 +14,25 @@ namespace DesignPatterns
             mediator.AddUser(premiumUser);
 
             normalUser.ChangeSubscription();
+        }
+
+        public static void ShowObserver()
+        {
+            BaggageHandler provider = new BaggageHandler();
+            ArrivalsMonitor observer1 = new ArrivalsMonitor("BaggageClaimMonitor1");
+            ArrivalsMonitor observer2 = new ArrivalsMonitor("SecurityExit");
+
+            provider.BaggageStatus(712, "Detroit", 3);
+            observer1.Subscribe(provider);
+            provider.BaggageStatus(712, "Kalamazoo", 3);
+            provider.BaggageStatus(400, "New York-Kennedy", 1);
+            provider.BaggageStatus(712, "Detroit", 3);
+            observer2.Subscribe(provider);
+            provider.BaggageStatus(511, "San Francisco", 2);
+            provider.BaggageStatus(712);
+            observer2.Unsubscribe();
+            provider.BaggageStatus(400);
+            provider.LastBaggageClaimed();
         }
     }
 }
---
+++ src/C#/Observer/Unsubscriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Observer
+{
+    internal class Unsubscriber<BaggageInfo> : IDisposable
+    {
+        private List<IObserver<BaggageInfo>> _observers;
+        private IObserver<BaggageInfo> _observer;
+
+        internal Unsubscriber(List<IObserver<BaggageInfo>> observers, IObserver<BaggageInfo> observer)
+        {
+            this._observers = observers;
+            this._observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+}
